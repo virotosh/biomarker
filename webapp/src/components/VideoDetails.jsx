8512394e836a8310833a3ee35e6af29dc75fb792
viro <vuonghiit@gmail.com>
2026-02-12 17:06:49 +0200

    frame handle

--- webapp/src/components/VideoDetails.jsx
+++ webapp/src/components/VideoDetails.jsx
@@ -34,7 +34,7 @@ const VideoDetails = () => {
   const getData = async (e) => {
     try {
       const res = await axios.post(
-        "http://193.166.24.186:5000/api/physio",
+        "http://localhost:5000/api/physio",
         { index: e }
       );
       if (res.status === 200) {
